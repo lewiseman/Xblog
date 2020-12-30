6389dc6ec74dae7df06e03df4452b3da8687123f
lewiseman <lewisemann@gmail.com>
2020-12-30 22:12:28 +0300

    prepared app for heroku 0

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Xblog.wsgi --log-file -
--- Xblog/settings.py
+++ Xblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$9ui!m9y0m21&4&7r7(&^a4luk_9ppihbmdr*s!cg=48)au8r4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['x-blog.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = '/static/'
 
